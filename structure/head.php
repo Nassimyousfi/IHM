c7d9b542aa91ccbd59d08a37510158cddfa2aee9
Bakaniko <baka.niko@gmail.com>
2017-06-08 14:21:20 +0200

    Correction Head.php

--- structure/head.php
+++ structure/head.php
@@ -1,5 +1,5 @@
 <head>
-	<title></title>
+	<title>Opéra National de Paris</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Google Material Icons-->
@@ -7,5 +7,5 @@
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 	<!-- Custom CSS -->
-	<link rel="stylesheet" href="<?php echo $path_root ; ?>style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo $path_root ; ?>style.css" type="text/css" media="screen" >
 </head>
