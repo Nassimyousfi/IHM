aaba98fdc55846d0b9ff850c4b57af6844c46dd7
MinMinLight <cascio.email@gmail.com>
2017-06-09 19:10:53 +0200

    ajout JS

---
+++ structure/JS.php
@@ -0,0 +1,10 @@
+<!-- Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<!-- Font Awesome-->
+<script src="https://use.fontawesome.com/6e96b5da51.js"></script>
+<!-- Google Maps-->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDqgqboFmTx_a7rxzlrFPjZhRbDMzpUiy8"></script>
+<!-- Fonctions Javascript -->
+<script src="<?php echo $path_root ; ?>fonctions.js"></script>
