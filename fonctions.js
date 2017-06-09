e43bafc07fec736e74a5b35b8e047b77f17a6fcc
MinMinLight <cascio.email@gmail.com>
2017-06-09 18:59:18 +0200

    JS Update

--- fonctions.js
+++ fonctions.js
@@ -3,14 +3,23 @@ var map;
 
 function initMap() {
 	var mapOptions = {
-		center: new google.maps.LatLng(42.699674, 2.894788),
-		zoom: 11, mapTypeId: google.maps.MapTypeId.ROADMAP
+		center: new google.maps.LatLng(48.861924,  2.344654),
+		zoom: 13, mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var mapElement = document.getElementById('map');
 
 	map = new google.maps.Map(mapElement, mapOptions);
 
 	var infoWindow = new google.maps.InfoWindow;
+
+	var marker = new google.maps.Marker({
+		position: {lat: 48.871928, lng: 2.331612},
+		map: map
+	});
+		var marker = new google.maps.Marker({
+		position: {lat: 48.852405, lng: 2.370204},
+		map: map
+	});
 }
 
 google.maps.event.addDomListener(window, 'load', initMap); // Créé la carte lors du chargement de la page
