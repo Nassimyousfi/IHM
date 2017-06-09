f91e17df59909f9287b3e716492a6971eb24ee02
MinMinLight <cascio.email@gmail.com>
2017-06-09 19:12:08 +0200

    menu Update

--- structure/menu.php
+++ structure/menu.php
@@ -4,15 +4,18 @@
   </button>
   <a class="navbar-brand"  title="Opéra National de Paris" href="<?php echo $path_root ; ?>index.php" ><img class="" src="<?php echo $path_images ; ?>logo.svg"></a>
   <div class="collapse navbar-collapse" id="menu-deroulant">
-  <ul class="navbar-nav mx-auto">
+    <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="<?php echo $path_root ; ?>index.php"><i class="material-icons">&#xE88A;&nbsp;&nbsp;</i>Accueil</a>
+        <a class="nav-link" href="<?php echo $path_root ; ?>index.php"><i class="material-icons">&#xE88A;&nbsp;</i>Accueil</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="<?php echo $path_pages ; ?>programmation.php"><i class="material-icons">&#xE616;&nbsp;&nbsp;</i>Programmation</a>
+        <a class="nav-link" href="<?php echo $path_pages ; ?>programmation.php"><i class="material-icons">&#xE616;&nbsp;</i>Programmation</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="<?php echo $path_pages ; ?>infos.php"><i class="material-icons">&#xE88F;&nbsp;&nbsp;</i>Infos</a>
+        <a class="nav-link" href="<?php echo $path_pages ; ?>infos.php"><i class="material-icons">&#xE88F;&nbsp;</i>Infos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo $path_pages ; ?>connexion.php"><i class="material-icons">&#xE853;&nbsp;</i>Connexion</a>
       </li>
     </ul>
   </div>
